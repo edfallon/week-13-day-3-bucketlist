69cd6ed52ab644beb80d6568fefc231cc4e7eebe
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-20 16:26:45 +0100

    connected to server on localhost:3000

--- server.js
+++ server.js
@@ -7,25 +7,24 @@ server.use(parser.json());
 server.use(parser.urlencoded({extended: true}));
 server.use(express.static("client/build"));
 
-const MongClient = require("mongodb").MongClient;
+const MongoClient = require("mongodb").MongoClient;
 
 MongoClient.connect("mongodb://localhost:27017",
 function(err, client){
-  if(err){
+  if(err){// checking if there is an error
     console.log(err);
     return;
   }
 
-const db = client.db(“bucket_list”);
- console.log(“Connected to database!“);
+const db = client.db("bucket_list");//connecting to the database
+ console.log("Connected to database!");
 
 
 
 
 
-
-server.listen(3000, function(){
-  console.log(“Listening on port 3000”);
+server.listen(3000, function(){// starts the server when you are econnected to the database
+  console.log("Listening on port 3000");
 });
 
 });
